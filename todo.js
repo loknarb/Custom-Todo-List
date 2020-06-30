dc401188ac864cc5c1f06250970070b847ac8db0
Loknarb <brumbaughco@gmail.com>
2020-06-30 17:08:48 +0200

    added a ton of comments and fixed edit not working on fresh adds changed dupl to duplicate for readability

--- todo.js
+++ todo.js
@@ -1,36 +1,28 @@
-// TODO grow todoItem when hovered
-// TODO dblick to edit todoItem
-// TODO create edit box with one button
-// TODO add "enter" event listener that runs validateform func
-// TODO add  button click event listener that runs validateform func
-
-// * ^^ https://javascript.info/task/sliding-menu
-
 // * Selectors
 let body = document.querySelector("body");
 let unsortedList = document.createElement("ul");
 let todoInput = document.querySelector(".todoInput");
 let todoContainer = document.querySelector(".todoContainer");
 let todoBtn = document.querySelector(".todoBtn");
+
 let todoDropdown = document.querySelector(".todoDropdown");
-todoContainer.appendChild(unsortedList);
 let editableTodoForm = document.createElement("form");
 let editableTodoInput = document.createElement("input");
 let editBtn = document.createElement("button");
 
+todoContainer.appendChild(unsortedList);
 editableTodoForm.appendChild(editableTodoInput);
 editableTodoForm.appendChild(editBtn);
 
 // * Listeners
 
 if (document.readyState !== "loading") {
-    getTodos();
+    getLocalTodos();
 } else {
     document.addEventListener("DOMContentLoaded", function () {
         getLocalTodos();
     });
 }
-// document.addEventListener("DomContentLoaded", getLocalTodos());
 unsortedList.addEventListener("click", deleteItem);
 todoBtn.addEventListener("click", validateForm);
 todoDropdown.addEventListener("click", filterTodo);
@@ -74,7 +66,7 @@ function editForm(event) {
 function addTodo(event) {
     // ! Prevents form from submitting
     event.preventDefault();
-    if (duplicateLocalTodo(todoInput.value) != "dupl") {
+    if (duplicateLocalTodo(todoInput.value) != "duplicate") {
         // * Creates Todo that contains Check Trash and Submitted Input and Append to UL
         let todo = document.createElement("div");
         todo.classList.add("todo");
@@ -86,6 +78,8 @@ function addTodo(event) {
         todoItem.classList.add("todoItem");
         todoItem.innerText = todoInput.value;
         todo.appendChild(todoItem);
+        // * Adds Listener to only our LI Element in case of Editing
+        todoItem.addEventListener("click", editTodo);
         // * Create Trash Button
         let trashBtn = document.createElement("button");
         trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
@@ -116,9 +110,8 @@ function addEditTodo(event) {
     let todoBody = parentForm.parentElement;
 
     //* Change Previous Text to Current Text
-    if (duplicateLocalTodo(editableTodoInput.value) != "dupl") {
+    if (duplicateLocalTodo(editableTodoInput.value) != "duplicate") {
         parentForm.classList.remove("inputError");
-        //  TODO -- Add our edit in sub of currentAdds our current Input to Local Storage
         editLocalTodos(todoBody, editableTodoInput.value);
         todoBody.innerText = editableTodoInput.value;
         // * Pushes current variation and removes old variation
@@ -158,33 +151,41 @@ function deleteItem(event) {
 
 function editTodo(event) {
     let todoItem = event.target;
+    // * Adding Editable Form to clicked on LI Element
     todoItem.appendChild(editableTodoForm);
+    // * Adding Classes/Attributes to Form, Input Field, and Button Elements
     editableTodoForm.classList.remove("inputError");
     editableTodoForm.classList.add("editableForm");
     editableTodoInput.classList.add("editableInput");
+    editBtn.classList.add("editBtn");
     editableTodoForm.setAttribute("onsubmit", "return editForm()");
     editableTodoForm.setAttribute("name", "editForm");
     editableTodoInput.setAttribute("name", "edit");
-    editBtn.innerHTML = '<i class="fas fa-edit"></i>';
-    editBtn.classList.add("editBtn");
-
-    editBtn.setAttribute("type", "submit");
     editableTodoInput.setAttribute("type", "text");
+    editBtn.setAttribute("type", "submit");
+    editBtn.innerHTML = '<i class="fas fa-edit"></i>';
+
     if (todoItem.classList[0] === "todoItem") {
+        // * Changes our LI items value to our Input Field
         editableTodoInput.value = todoItem.childNodes[0].textContent;
+        // * Adds 3 seperate listeners that Listen for clicking out of target LI or clicking inside target LI's edit Input Field and selecting its Button Element
         editBtn.addEventListener("click", editForm);
         todoContainer.addEventListener("focusout", removeEditTodo);
         body.addEventListener("focusout", removeEditTodo);
     }
 }
+// * Removes TodoForm Element from targetted LI Element
 function removeEditTodo(event) {
     editableTodoForm.remove();
 }
-
+// * Category selector
 function filterTodo(event) {
     let todos = unsortedList.childNodes;
+    // * Iterates thru every single todo
     todos.forEach(function (todo) {
+        // * Targets the Category name and switches function targets that specific class
         switch (event.target.className) {
+            // * similar to if className === "filterTodo"
             case "filterTodo":
                 if (!todo.classList.contains("trashed")) {
                     todo.style.display = "flex";
@@ -230,6 +231,7 @@ function saveLocalTodos(saveTodo) {
         //* Checks that it is already in localStorage grabs it and turns it back into an array
         todos = JSON.parse(localStorage.getItem("todos"));
     }
+    // * Automatically creates element with second value being uncompleted
     let todoItem = {
         todo: saveTodo,
         todoStatus: "uncompleted",
@@ -238,7 +240,7 @@ function saveLocalTodos(saveTodo) {
     todos.push(todoItem);
     localStorage.setItem("todos", JSON.stringify(todos));
 }
-
+// * Add
 function getLocalTodos() {
     let todos;
     //* Check if already saved
@@ -258,9 +260,8 @@ function getLocalTodos() {
         let todoItem = document.createElement("li");
         todoItem.classList.add("todoItem");
         todoItem.innerText = savedTodo["todo"];
-        // savedTodo;
         todoDiv.appendChild(todoItem);
-        // * Adds Edit Listener to only our LI
+        // * Adds Listener to only our LI Element in case of Editing
         todoItem.addEventListener("click", editTodo);
         // * Create Trash Button
         let trashBtn = document.createElement("button");
@@ -272,13 +273,13 @@ function getLocalTodos() {
         compBtn.innerHTML = '<i class="fas fa-check"></i>';
         compBtn.classList.add("compBtn");
         todoDiv.appendChild(compBtn);
-
         unsortedList.appendChild(todoDiv);
-        let status = savedTodo.todoStatus;
-        if (status === "completed") {
+        // * Creates variable todoStatus that automatically reassigns previous saved LocalStorage status
+        let todostatus = savedTodo.todoStatus;
+        if (todostatus === "completed") {
             todoDiv.classList.toggle("completed");
         }
-        if (status === "trashed") {
+        if (todostatus === "trashed") {
             todoDiv.classList.toggle("trashed");
         }
     });
@@ -293,6 +294,7 @@ function markCompleted(todo) {
     }
     const todoIndex = todo.children[0].innerText;
     let index = todos.findIndex((obj) => obj.todo == todoIndex);
+    // * Reassigns todoStatus variable to completed if was previously uncompleted
     if (todos[index].todoStatus === "uncompleted") {
         todos[index].todoStatus = "completed";
         localStorage.setItem("todos", JSON.stringify(todos));
@@ -311,12 +313,13 @@ function markTrashed(todo) {
     }
     const todoIndex = todo.children[0].innerText;
     let index = todos.findIndex((obj) => obj.todo == todoIndex);
+    // * Reassigns todoStatus variable to trashed if was previously uncompleted
     if (todos[index].todoStatus === "uncompleted") {
         todos[index].todoStatus = "trashed";
         localStorage.setItem("todos", JSON.stringify(todos));
+        // * Completely removes todo item from LocalStorage and from HTML
     } else if (todos[index].todoStatus === "trashed") {
         removeLocalTodos(todo);
-        // TODO Figure out how this works? \/\/
         todo.remove();
     } else {
         todos[index].todoStatus = "uncompleted";
@@ -333,6 +336,7 @@ function removeLocalTodos(todo) {
     }
     const todoIndex = todo.children[0].innerText;
     let index = todos.findIndex((obj) => obj.todo == todoIndex);
+    // * Targets specific todo removes it then updates LocalStorage
     todos.splice(index, 1);
     localStorage.setItem("todos", JSON.stringify(todos));
 }
@@ -344,13 +348,14 @@ function duplicateLocalTodo(todoInput) {
     } else {
         todos = JSON.parse(localStorage.getItem("todos"));
     }
+    // * Loops thru entire Todos searching for similar Input Submission, returns duplicate to prevent form submission if found
     for (let i = 0; i < todos.length; i++) {
         if (todos[i].todo === todoInput) {
-            return "dupl";
+            return "duplicate";
         }
     }
 }
-
+// * Requires old and current Input Submission values
 function editLocalTodos(todo, editTodo) {
     let todos;
     //* Check if already saved
@@ -364,12 +369,8 @@ function editLocalTodos(todo, editTodo) {
     // * Finds todoItem text and isolates replaces its value to current
     const todoIndex = todo.innerText;
     let index = todos.findIndex((obj) => obj.todo == todoIndex);
-    // if (todos[index].todo === editTodo) {
-    //     return "dupl";
-    // } else {
+    // * Replaces old todo with edited Todo
     todos[index].todo = editTodo;
     localStorage.setItem("todos", JSON.stringify(todos));
     //* Pushes todo into array Todos then creates JSON object with key todos and value array todos
-    // }
 }
-// TODO remove section
